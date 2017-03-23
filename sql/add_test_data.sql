c560464a842f461dd44d8175f166d04948f76aa4
8Cookie9 <jaakko.koli@gmail.com>
2017-03-23 23:30:32 +0200

    week 2

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,7 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja(nimi, oikeudet) VALUES ('user1','regular');
+INSERT INTO Kayttaja(nimi, oikeudet) VALUES ('user2','regular');
+INSERT INTO Kayttaja(nimi, oikeudet) VALUES ('admin','admin');
+INSERT INTO Aihealue(nimi) VALUES ('Yleistä');
+INSERT INTO Keskustelu(keskustelu_id, kayttaja_id, otsikko) VALUES (1,1,'Yleinen keskustelu');
+INSERT INTO Viesti(kayttaja_id, sisalto) VALUES (1,'Random message');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,30 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Aihealue(
+	id SERIAL PRIMARY KEY,
+	nimi varchar(32) NOT NULL
+);
+
+CREATE TABLE Kayttaja(
+	id SERIAL PRIMARY KEY,
+	nimi varchar(16) NOT NULL,
+	oikeudet varchar(10) NOT NULL
+);
+
+CREATE TABLE Keskustelu(
+	id SERIAL PRIMARY KEY,
+	keskustelu_id INTEGER REFERENCES Keskustelu(id),
+	kayttaja_id INTEGER REFERENCES Kayttaja(id),
+	otsikko varchar(32) NOT NULL
+);
+
+CREATE TABLE Viesti(
+	id SERIAL PRIMARY KEY,
+	viesti_id INTEGER REFERENCES Viesti(id),
+	kayttaja_id INTEGER REFERENCES Kayttaja(id),
+	sisalto varchar(1000) NOT NULL
+);
+
+CREATE TABLE Luettu(
+	keskustelu_id INTEGER REFERENCES Keskustelu(id),
+	kayttaja_id INTEGER REFERENCES Kayttaja(id),
+);
