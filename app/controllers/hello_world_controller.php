5c394c156895ee7313a72446d2b02833f5faa0bf
8Cookie9 <jaakko.koli@gmail.com>
2017-03-23 19:58:34 +0200

    views

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,12 +4,27 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  // View::make('home.html');
-	  echo 'Tämä on etusivu!';
+   	  View::make('home.html');
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
       echo 'Hello World!';
+    }
+	
+	public static function aihealueet(){
+   	  View::make('suunnitelmat/aihealueet.html');
+    }
+	
+	public static function keskustelut(){
+   	  View::make('suunnitelmat/keskustelut.html');
+    }
+	
+	public static function keskustelu(){
+   	  View::make('suunnitelmat/keskustelu.html');
+    }
+	
+	public static function login(){
+   	  View::make('suunnitelmat/login.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,20 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/aihealueet', function() {
+    HelloWorldController::aihealueet();
+  });
+  
+  $routes->get('/keskustelut', function() {
+    HelloWorldController::keskustelut();
+  });
+  
+  
+  $routes->get('/keskustelu', function() {
+    HelloWorldController::keskustelu();
+  });
+  
+  $routes->get('/login', function() {
+    HelloWorldController::login();
+  });
