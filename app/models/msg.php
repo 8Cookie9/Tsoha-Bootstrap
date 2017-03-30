ea0f83c28b1b6ca5c4fb19a781a26e7b6e3ff31b
8Cookie9 <jaakko.koli@gmail.com>
2017-03-30 17:48:23 +0300

    testing

--- app/models/msg.php
+++ app/models/msg.php
@@ -1,43 +1,44 @@
-class Msg extends BaseModel{
-	public $id, $thread_id, $user_id, $ans_id, $content;
-	
-	public function __construct($attributes){
-		parent::__construct($attributes);
-	}
-	
-	public static function all(){
-		$query = DB::connection()->prepare('SELECT * FROM Viesti');
-		$query->execute();
-		$rows = $query->fetchAll();
-		$messages = array();
-
-		foreach($rows as $row){
-			$messages[] = new Msg(array(
-				'id' => $row['id'],
-				'thread_id' => $row['thread_id'],
-				'user_id' => $row['user_id'],
-				'ans_id' => $row['ans_id'],
-				'content' => $row['content']
-			));
+<?php
+	class Msg extends BaseModel{
+		public $id, $thread_id, $user_id, $ans_id, $content;
+		
+		public function __construct($attributes){
+			parent::__construct($attributes);
 		}
+		
+		public static function all(){
+			$query = DB::connection()->prepare('SELECT * FROM Viesti');
+			$query->execute();
+			$rows = $query->fetchAll();
+			$messages = array();
 
-		return $messages;
+			foreach($rows as $row){
+				$messages[] = new Msg(array(
+					'id' => $row['id'],
+					'thread_id' => $row['thread_id'],
+					'user_id' => $row['user_id'],
+					'ans_id' => $row['ans_id'],
+					'content' => $row['content']
+				));
+			}
+
+			return $messages;
+		}
+		
+		public static function find($id){
+			$query = DB::connection()->prepare('SELECT * FROM Viesti WHERE id = :id LIMIT 1');
+			$query->execute(array('id' => $id));
+			$row = $query->fetch();
+			if($row){
+				$msg = new Game(array(
+					'id' => $row['id'],
+					'thread_id' => $row['thread_id'],
+					'user_id' => $row['user_id'],
+					'ans_id' => $row['ans_id'],
+					'content' => $row['content']
+				));
+			return $msg;
+		}
+		return null;
+	  }
 	}
-	
-	public static function find($id){
-		$query = DB::connection()->prepare('SELECT * FROM Viesti WHERE id = :id LIMIT 1');
-		$query->execute(array('id' => $id));
-		$row = $query->fetch();
-		if($row){
-			$msg = new Game(array(
-				'id' => $row['id'],
-				'thread_id' => $row['thread_id'],
-				'user_id' => $row['user_id'],
-				'ans_id' => $row['ans_id'],
-				'content' => $row['content']
-			));
-		return $msg;
-    }
-    return null;
-  }
-}
