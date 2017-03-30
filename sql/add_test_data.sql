ab3dd162c86190a6b038b7ed98dee7cc31865c5b
8Cookie9 <jaakko.koli@gmail.com>
2017-03-30 21:35:43 +0300

    testing

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -5,4 +5,4 @@ INSERT INTO Kayttaja(nimi, admin) VALUES ('admin',true);
 INSERT INTO Aihealue(nimi) VALUES ('Yleistä');
 INSERT INTO Aihealue(nimi) VALUES ('Harrastukset');
 INSERT INTO Keskustelu(aihealue_id, kayttaja_id, otsikko) VALUES (1,1,'Yleinen keskustelu');
-INSERT INTO Viesti(keskustelu_id, kayttaja_id, sisalto) VALUES (1, 1,'Random message');
+INSERT INTO Viesti(keskustelu_id, kayttaja_id, sisalto) VALUES (1,1,'Random message');
